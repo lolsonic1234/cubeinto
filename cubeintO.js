48281552f3143a5213a2a7ca8bb64e79bc2f9395
lolsonic1234 <anthonydaluz@hotmail.com>
2016-08-31 14:56:25 -0400

    Update cubeintO.js

--- cubeintO.js
+++ cubeintO.js
@@ -10,6 +10,13 @@ var neg = "yes";
 
 var curbox = 1; 
 
+var box1Cubes = 1;
+var box1limit = load('b1l');
+
+var box2Cubes = 0;
+var box2limit = load('b2l');
+
+
 // Variables
 var stage = load('stage');
 var name = load('name');
@@ -62,6 +69,8 @@ MAINsystem["tech"] = {
 // images
 
 var mainCube = "CubeintO - Main Cube.png";
+var fuseCubeL = "CubeintO - FusedCube Half (1).png";
+var fuseCubeR = "CubeintO - FusedCube Half (2).png";
 
 // Initiantion Function
 
@@ -69,7 +78,9 @@ function intin(){
     document.cookie = "stage=1";
     document.cookie = "name=" + $('.Enteryourname').val();
     document.cookie = "cubeColor=blue";
-    document.cookie = "gash=1";
+    document.cookie = "gash=0";
+    document.cookie = "b1l=5";
+    document.cookie = "b2l=5";
     document.cookie = "as=no";
     document.cookie = "at=no";
     document.cookie = "af=no";
@@ -170,6 +181,7 @@ $('.leftArrow').click(function(){
 $('.resumeGameDiv').click(function(){
     cubeColor = "blue";
     fadeIt();
+    updateCubeBuy();
     window.setTimeout(function(){
         $('.spawnFirstTime').css({
             zIndex: "-1",
@@ -289,7 +301,7 @@ function alertIt(textToAlert){
     $('.Alerter').css({
         position: "absolute",
         opacity: "0.75",
-        zIndex: "10",
+        zIndex: "100",
     });
     $('.alertinfo').html(textToAlert);
     window.setTimeout(function(){
@@ -307,6 +319,18 @@ function alertIt(textToAlert){
     }, 3000);
 }
 
+
+function updateCubeBuy(){
+    MAINsystem["cubes"]["scube"] = load('bs');
+    
+    if(gash >= 10 || MAINsystem["cubes"]["scube"] == "yes"){
+        document.cookie = "bs=yes";
+        $('.containerShopS').css({
+            opacity: "1",
+            left: "0vw"
+        })
+    }
+}
 
 
 function fadeIt(){
@@ -477,16 +501,118 @@ $('.Xbutton').click(function(){
     };
 });
 
-function spawnCube(type, box, locationX, locationY, color, cash){
+
+
+$('.cubeimg').click(function(){
+    box1limit = load('b1l');
     
+    var randomX = Math.floor(Math.random() * 32) + 2;
+    var randomY = Math.floor(Math.random() * 32) + 2;
+    var randomDeg = Math.floor(Math.random() * 360) + 0;
+    
+    var type = mainCube;
+    var box = "1";
+    var cash = "cash1";
+    var color = "blue";
+    
+    if(gash >= 1 && box1Cubes < box1limit){
+        spawnCube(type, box, randomX, randomY, color, cash, randomDeg, "no");
+        document.cookie = "gash=" + (gash - 1);
+        gash = load("gash");
+        box1Cubes += 1;
+    } else if( box1Cubes >= box1limit){
+        alertIt('You can only have a maximum of.. <br>' + box1limit + ' cubes of that kind.')
+    } else if(gash < 1){
+        alertIt('You dont have enough cash.')
+    }
+})
+
+$('.cubeimgS').click(function(){
+    box2limit = load('b1l');
+    
+    var randomX = Math.floor(Math.random() * 32) + 2;
+    var randomY = Math.floor(Math.random() * 32) + 2;
+    var randomDeg = Math.floor(Math.random() * 360) + 0;
+    
+    var type = "split";
+    var box = "2";
+    var cash = "cash2";
+    var color = "none";
+    
+    if(gash >= 10 && box1Cubes < box1limit){
+        spawnCube(type, box, randomX, randomY, color, cash, randomDeg, "split");
+        document.cookie = "gash=" + (gash - 10);
+        gash = load("gash");
+        box2Cubes += 1;
+    } else if( box2Cubes >= box2limit){
+        alertIt('You can only have a maximum of.. <br>' + box2limit + ' cubes of that kind.')
+    } else if(gash < 10){
+        alertIt('You dont have enough cash.')
+    }
+})
+
+
+
+function spawnCube(type, box, locationX, locationY, color, cash, deg, special){
+    if(special == "no"){
+        $('.box' + box).append('<div class="' + cash + '" style="position: absolute; width: 4vw; height: 4vw; background-color: ' + color + '; top: ' + locationY + 'vw; left: ' + locationX + 'vw; background-image: url(' + type + '); background-repeat: no-repeat; background-size: cover; transform: rotate(' + deg + 'deg); transition: transform 1s, background-color 1s, top 1s, left 1s, width 1s, height 1s, opacity 1s;;"></div>');
+    } else if(special == "split"){
+        $('.box' + box).append('<div class="' + cash + '" style="position: absolute; width: 4vw; height: 4vw; top: ' + locationY + 'vw; left: ' + locationX + 'vw; transform: rotate(' + deg + 'deg); transition: transform 1s, background-color 1s, top 1s, left 1s, width 1s, height 1s; opacity 1s;"><div class="hl" style="background-image: url("' + fuseCubeL + '"); background-repeat: no-repeat; background-size: cover; position: absolute; height: 4vw; width: 4vw;"></div><div class="hr" style="background-image: url("' + fuseCubeR + '"); background-repeat: no-repeat; background-size: cover; position: absolute; height: 4vw; width: 4vw;"></div></div>')
+    };
+};
+
+
+$(document).on('mouseenter', '.cash1', function(e) {
+    document.cookie = "gash=" + ((gash - 1) + 3);
+    gash = load("gash");
+    animateRem = this;
+    animator(animateRem)
+    box1Cubes -= 1;
+    updateCubeBuy();
+});
+
+function animator(animkill){
+    $(animkill).attr("class", "ded")
+    $(animkill).css({
+        transform: "rotate(1000deg)",
+        height: "0vw",
+        width: "0vw"
+    })
+    
+    window.setTimeout(function(){
+        animkill.remove();
+    }, 1000);
 }
 
 
+$(document).on('mouseenter', '.cash2', function(e) {
+    document.cookie = "gash=" + ((gash - 1) + 12);
+    gash = load("gash");
+    animateRem = this;
+    animator(animateRem)
+    box2Cubes -= 1;
+    updateCubeBuy();
+});
 
-
-
-
-
+function animatorSpl(animkill){
+    $(animkill).attr("class", "ded")
+    $(animkill).css({
+        transform: "rotate(0deg)",
+        opacity: "0"
+    })
+    
+    $(animkill + ".hfl").css({
+        left: "-=10"
+    })
+    
+    $(animkill + ".hfr").css({
+        left: "+=10"
+    })
+    
+    window.setTimeout(function(){
+        animkill.remove();
+    }, 1000);
+}
 
 
 
